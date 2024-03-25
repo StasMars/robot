709d6b1e3386e17c6a9e34d1f4d772d2bdc9dae3
Stas Mars <stasskywalker@gmail.com>
2024-03-25 17:58:51 +0300

    Create FastAPI app

--- backend.py
+++ backend.py
@@ -1,4 +1,31 @@
 from fastapi import FastAPI
-from subprocess import Popen, PIPE
+from fastapi.responses import HTMLResponse
+from robot import robot
+import asyncio
 
 app = FastAPI()
+robot_queue = asyncio.Queue()
+
+@app.get('/')
+async def greeting():
+    content = """
+<body>
+    <h2>Добрый день! Меня зовут Станислав</h2>
+    <p>Это мое тестовое задание для ИТ-стажировки Гринатом (Python разработчик RPA)</p>
+    <p><a href="http://127.0.0.1:8000/docs">Ссылка на Swagger UI</a></p>
+</body>
+    """
+    return HTMLResponse(content=content)
+
+async def start_robot(start_from: int):
+    await robot(start_from, robot_queue)
+
+@app.post("/start_robot/")
+async def start_robot_endpoint(start_from: int = 0):
+    asyncio.create_task(start_robot(start_from))  # Запуск асинхронной функции robot с указанным start_from
+    return {"message": "Robot started"}
+
+@app.post("/stop_robot/")
+async def stop_robot_endpoint():
+    robot_queue.put_nowait("stop")   # Помещаем сообщение "stop" в очередь
+    return {"message": "Robot stopping"}
--- robot.py
+++ robot.py
@@ -1,14 +1,14 @@
 import asyncio
-import sys
 
-async def robot(start_from=0):
-    count = start_from
+async def robot(start_from=0, queue=None):
+    count_value = start_from
+    print('Добрый день, меня зовут Станислав и это мое тестовое задание для ИТ-стажировки Гринатом (Python разработчик RPA)')
     while True:
-        print(count)
-        count += 1
+        print(count_value)
+        count_value += 1
         await asyncio.sleep(1)
-
-
-if __name__ == "__main__":
-    start_from = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-    asyncio.run(robot(start_from))
+        if queue and not queue.empty():
+            message = await queue.get()
+            if message == "stop":
+                print("Robot Stopped. Mission Complete :)")
+                break
