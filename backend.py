a9e1ab345c3407ae79b56944c16e1a991ee70a13
Stas Mars <stasskywalker@gmail.com>
2024-03-25 20:01:04 +0300

    Add infromation about all robot's starts and Add table in html file about all this starts

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import sqlite3
+from datetime import datetime
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from pathlib import Path
 from robot import robot
@@ -10,14 +12,40 @@ app = FastAPI(
 )
 robot_queue = asyncio.Queue()
 
+
+conn = sqlite3.connect('robot_data.db') # Подключение к БД
+c = conn.cursor()
+
+
+c.execute('''CREATE TABLE IF NOT EXISTS robot_runs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            start_time TEXT,
+            duration INTEGER,
+            start_from INTEGER
+            )'''
+        )
+
+# Функция для записи информации о запуске робота в базу данных
+def log_robot_run(start_time, duration, start_from):
+    c.execute("INSERT INTO robot_runs (start_time, duration, start_from) VALUES (?, ?, ?)",
+              (start_time, duration, start_from))
+    conn.commit()
+
+
+
+
 @app.get('/', tags=["Приветствие"])
 async def greeting():
-    html_file_path = Path(__file__).parent / 'index.html'  # Путь к файлу HTML
+    html_file_path = Path(__file__).parent / 'templates/index.html'  # Путь к файлу HTML
     return FileResponse(html_file_path)
 
 
 async def start_robot(start_from: int):
+    start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     await robot(start_from, robot_queue)
+    end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    duration = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S") - datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    log_robot_run(start_time, duration.total_seconds(), start_from)
 
 
 @app.post("/start_robot/", tags=["Работа с роботом"], summary="Запуск робота")
@@ -38,3 +66,13 @@ async def stop_robot_endpoint(): # Остановка робота (Помеща
     """
     robot_queue.put_nowait("stop")   
     return {"message": "Robot stopping"}
+
+@app.get("/robot_runs/", tags=["Информация о запусках робота"], summary="Получение информации о запусках робота")
+async def get_robot_runs():                    # Вывод информации о запусках робота
+    """
+    Выводит информацию о всех запусках робота.\n
+    Нажмите кнопку Execute для получения подробной информации. Спасибо.
+    """
+    c.execute("SELECT * FROM robot_runs")
+    runs = c.fetchall()
+    return {"robot_runs": runs}
